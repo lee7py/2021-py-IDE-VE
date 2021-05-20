d3cf15c11e84b247625acdc5af96b2917da679a3
lee7py <64510595+lee7py@users.noreply.github.com>
2021-05-20 12:28:01 +0900

    upload

---
+++ py code/pd serise 테스트.py
@@ -0,0 +1,11 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+df = pd.DataFrame(np.random.randn(1000, 4), columns=['A', 'B', 'C', 'D'],
+                  index=pd.date_range('1/1/2020', periods=1000))
+df = df.cumsum()
+print(df.head())
+
+df.plot()
+plt.show()
---
+++ py code/pybasic.py
@@ -0,0 +1,8 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+x = np.random.random(10)
+print(x)
+
+plt.plot(x)
+plt.show()
